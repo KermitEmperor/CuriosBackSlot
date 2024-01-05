9f02a1a9e0a43eb6c60417f06f306b2a9db555de
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2024-01-05 13:17:45 +0100

    Player model is now rendered in the back customization screen

--- src/main/java/com/kermitemperor/curiosbackslot/CuriosBackSlot.java
+++ src/main/java/com/kermitemperor/curiosbackslot/CuriosBackSlot.java
@@ -1,30 +1,24 @@
 package com.kermitemperor.curiosbackslot;
 
 import com.kermitemperor.curiosbackslot.capability.XYZPosAndRotation;
-import com.kermitemperor.curiosbackslot.capability.XYZPosAndRotationProvider;
 import com.kermitemperor.curiosbackslot.client.BackWeaponConfigurationScreen;
 import com.kermitemperor.curiosbackslot.client.KeyBinding;
 import com.kermitemperor.curiosbackslot.config.ClientConfig;
+import com.kermitemperor.curiosbackslot.event.CapabilityEvent;
 import com.kermitemperor.curiosbackslot.network.PacketChannel;
-import com.kermitemperor.curiosbackslot.network.packet.ResyncWithMePacket;
 import com.kermitemperor.curiosbackslot.network.packet.SwitchPacket;
 import com.kermitemperor.curiosbackslot.render.GuiRenderer;
 import com.kermitemperor.curiosbackslot.util.CuriosBackSlotHandler;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.gui.OverlayRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -45,7 +39,6 @@ import top.theillusivec4.curios.api.event.CurioEquipEvent;
 public class CuriosBackSlot {
 
     public static final String MOD_ID = "curiosbackslot";
-
     @SuppressWarnings("unused")
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -60,6 +53,7 @@ public class CuriosBackSlot {
 
         IEventBus ForgeEventBus = MinecraftForge.EVENT_BUS;
         ForgeEventBus.register(this);
+        ForgeEventBus.register(new CapabilityEvent());
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ClientConfig.SPEC, MOD_ID + "-client.toml");
     }
@@ -71,8 +65,7 @@ public class CuriosBackSlot {
     private void clientSetup(final FMLClientSetupEvent event) {
         OverlayRegistry.registerOverlayTop(CuriosBackSlotHandler.SLOT_ID, new GuiRenderer());
         ClientRegistry.registerKeyBinding(KeyBinding.SWITCHING_KEY);
-        ClientRegistry.registerKeyBinding(KeyBinding.SWITCHING_KEY);
-
+        ClientRegistry.registerKeyBinding(KeyBinding.XYZ_KEY);
     }
 
     private void enqueue(final InterModEnqueueEvent evt) {
@@ -87,7 +80,6 @@ public class CuriosBackSlot {
     }
 
     public void onRegisterCapabilities(final RegisterCapabilitiesEvent event) {
-        LOGGER.info("registered");
         event.register(XYZPosAndRotation.class);
     }
 
@@ -109,58 +101,9 @@ public class CuriosBackSlot {
             Minecraft.getInstance().pushGuiLayer(new BackWeaponConfigurationScreen());
         }
 
+        // This shouldn't be implemented manually
         if (mc.screen instanceof BackWeaponConfigurationScreen) {
             mc.mouseHandler.releaseMouse();
-        }
-    }
-
-    @SubscribeEvent
-    public void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof Player player) {
-
-            LazyOptional<XYZPosAndRotation> existingCapability = player.getCapability(XYZPosAndRotationProvider.PLAYER_BACK_WEAPON_XYZ);
-
-            final XYZPosAndRotationProvider provider = new XYZPosAndRotationProvider();
-
-            if (!existingCapability.isPresent()) {
-                LOGGER.info("attached, is Client: " + player.level.isClientSide());
-                event.addCapability(new ResourceLocation(MOD_ID, "properties"), provider);
-
-                //This event is called twice, one on Server and on Client, we have to ask the server to
-                //send a packet about the player's capability
-                //and to set it in our side
-
-                if (player.level.isClientSide) {
-
-                    //We have to use the execute method, otherwise the PacketChannel will be called too soon
-
-                    Minecraft.getInstance().execute(() -> PacketChannel.sendToServer(new ResyncWithMePacket()));
-                }
-            } else {
-                LOGGER.info("XYZPosAndRotation capability already present");
-            }
-        }
-    }
-
-    @SuppressWarnings("CodeBlock2Expr")
-    @SubscribeEvent
-    public void onPlayerCloned(PlayerEvent.Clone event) {
-        if(event.isWasDeath()) {
-            event.getOriginal().getCapability(XYZPosAndRotationProvider.PLAYER_BACK_WEAPON_XYZ).ifPresent(oldStore -> {
-                event.getOriginal().getCapability(XYZPosAndRotationProvider.PLAYER_BACK_WEAPON_XYZ).ifPresent(newStore -> {
-                    newStore.copyFrom(oldStore);
-                });
-            });
-        }
-    }
-
-
-    @SubscribeEvent
-    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if(event.side != null) {
-            event.player.getCapability(XYZPosAndRotationProvider.PLAYER_BACK_WEAPON_XYZ).ifPresent(xyzPosAndRotation -> {
-                    //LOGGER.info(String.valueOf(xyzPosAndRotation.getX()) + event.side);
-            });
         }
     }
 
--- src/main/java/com/kermitemperor/curiosbackslot/client/BackWeaponConfigurationScreen.java
+++ src/main/java/com/kermitemperor/curiosbackslot/client/BackWeaponConfigurationScreen.java
@@ -2,20 +2,30 @@ package com.kermitemperor.curiosbackslot.client;
 
 import com.kermitemperor.curiosbackslot.network.PacketChannel;
 import com.kermitemperor.curiosbackslot.network.packet.RenderInfoCapabilityPacket;
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
+
 
 import static com.kermitemperor.curiosbackslot.CuriosBackSlot.LOGGER;
 
 public class BackWeaponConfigurationScreen extends Screen {
     public BackWeaponConfigurationScreen() {
-
         super(new TextComponent("yeah"));
     }
 
-    protected Minecraft mc = super.minecraft;
+
 
     private double x;
     private double y;
@@ -50,9 +60,84 @@ public class BackWeaponConfigurationScreen extends Screen {
     }
 
     @Override
+    public void render(@NotNull PoseStack pMatrixStack, int pMouseX, int pMouseY, float pPartialTicks) {
+        this.renderBackground(pMatrixStack);
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player != null) {
+            renderEntity(this.width / 2, this.height / 2 + 25, 30, pMouseX - (float) this.width / 2, (float) (this.height / 2 - 25) - pMouseY, player);
+        }
+        super.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
+    }
+
+
+    //Hi-pity Hopi ty, your code is my property
+    @SuppressWarnings("deprecation")
+    public static void renderEntity(int pPosX, int pPosY, int pScale, float pMouseX, float pMouseY, LivingEntity pLivingEntity) {
+        float f = (float)Math.atan(pMouseX / 40.0F);
+        float f1 = (float)Math.atan(pMouseY / 40.0F);
+        PoseStack posestack = RenderSystem.getModelViewStack();
+        posestack.pushPose();
+        posestack.translate(pPosX, pPosY, 1050.0D);
+        posestack.scale(1.0F, 1.0F, -1.0F);
+        RenderSystem.applyModelViewMatrix();
+        PoseStack posestack1 = new PoseStack();
+        posestack1.translate(0.0D, 0.0D, 1000.0D);
+        posestack1.scale((float)pScale, (float)pScale, (float)pScale);
+        Quaternion quaternion = Vector3f.ZP.rotationDegrees(180.0F);
+        Quaternion quaternion1 = Vector3f.XP.rotationDegrees(f1 * 20.0F);
+        quaternion.mul(quaternion1);
+        posestack1.mulPose(quaternion);
+        posestack1.mulPose(Vector3f.YP.rotationDegrees(180f));
+        float f2 = pLivingEntity.yBodyRot;
+        float f3 = pLivingEntity.getYRot();
+        float f4 = pLivingEntity.getXRot();
+        float f5 = pLivingEntity.yHeadRotO;
+        float f6 = pLivingEntity.yHeadRot;
+        pLivingEntity.yBodyRot = 180.0F + f * 20.0F;
+        pLivingEntity.setYRot(180.0F + f * 40.0F);
+        pLivingEntity.setXRot(-f1 * 20.0F);
+        pLivingEntity.yHeadRot = pLivingEntity.getYRot();
+        pLivingEntity.yHeadRotO = pLivingEntity.getYRot();
+        Lighting.setupForEntityInInventory();
+        EntityRenderDispatcher entityrenderdispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        quaternion1.conj();
+        entityrenderdispatcher.overrideCameraOrientation(quaternion1);
+        entityrenderdispatcher.setRenderShadow(false);
+        MultiBufferSource.BufferSource multibuffersource$buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
+        RenderSystem.runAsFancy(() ->
+                entityrenderdispatcher.render(pLivingEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, posestack1, multibuffersource$buffersource, 15728880)
+        );
+        multibuffersource$buffersource.endBatch();
+        entityrenderdispatcher.setRenderShadow(true);
+        pLivingEntity.yBodyRot = f2;
+        pLivingEntity.setYRot(f3);
+        pLivingEntity.setXRot(f4);
+        pLivingEntity.yHeadRotO = f5;
+        pLivingEntity.yHeadRot = f6;
+        posestack.popPose();
+        RenderSystem.applyModelViewMatrix();
+        Lighting.setupFor3DItems();
+    }
+
+    @Override
+    public boolean keyPressed(int pKeyCode, int pScanCode, int pModifiers) {
+        if (pKeyCode == KeyBinding.XYZ_KEY.getKey().getValue()) {
+            this.onClose();
+            return true;
+        }
+        return super.keyPressed(pKeyCode, pScanCode, pModifiers);
+    }
+
+    @Override
     public boolean isPauseScreen() {
         return false;
     }
 
+    @Override
+    public void onClose() {
+        LOGGER.info("saving");
+        PacketChannel.sendToServer(new RenderInfoCapabilityPacket(x, y, 0, 0, 0, 0));
+        super.onClose();
+    }
 
 }
